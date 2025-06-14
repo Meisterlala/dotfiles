0a67e5b211bb1508adebc86ad3d8f7fed78c2639
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-06-14 13:06:15 +0200

    Update .config/sxhkd/wp_toggle.sh

--- dot_config/sxhkd/executable_wp_toggle.sh
+++ dot_config/sxhkd/executable_wp_toggle.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 if wp get wp-dir | grep -q 'nsfw'; then
-	wp wp-dir "~/Wallpapers/sfw/"
+	wp wp-dir "/home/ninja/Wallpapers/sfw/"
 	dunstify -h string:x-dunst-stack-tag:wallpaper -a "Wallpaper" "Displaying SFW Wallpapers" 
 else
-	wp wp-dir "~/Wallpapers/nsfw/"
+	wp wp-dir "/home/ninja/Wallpapers/nsfw/"
 	dunstify -h string:x-dunst-stack-tag:wallpaper -a "Wallpaper" "Displaying NSFW Wallpapers" 
 fi
