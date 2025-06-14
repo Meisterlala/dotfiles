fcf755edd1815231b6a3fcd921fb7bd5896fc7c3
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-06-14 13:00:46 +0200

    rename

--- dot_config/sxhkd/executable_wp_fallback.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-if wp get mode | grep -q 'Random'; then
-	wp mode static
-	dunstify -h string:x-dunst-stack-tag:wallpaper -a "Wallpaper" "Wallpaper locked" 
-else
-	wp mode random
-	dunstify -h string:x-dunst-stack-tag:wallpaper -a "Wallpaper" "Wallpaper unlocked" 
-fi
