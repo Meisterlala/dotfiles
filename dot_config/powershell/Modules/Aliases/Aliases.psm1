c5b270d9d04f8f6796436f67d97519c989658407
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-08 15:18:30 +0200

    refaktor

--- dot_config/powershell/Modules/Aliases/Aliases.psm1
+++ dot_config/powershell/Modules/Aliases/Aliases.psm1
@@ -44,3 +44,17 @@ if (Get-Command yay -ErrorAction SilentlyContinue) {
     Set-Alias -Name yyay -Value yay-no-confirm | Out-Null
 }
 
+function Measure-Script {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+    if (-not (Test-Path $Path)) {
+        throw "Script '$Path' does not exist."
+    }
+    $sw = [System.Diagnostics.Stopwatch]::StartNew()
+    try { & $Path } finally {
+        $sw.Stop();
+        Write-Host ("{0} ms" -f [math]::Round($sw.Elapsed.TotalMilliseconds))
+    }
+}
