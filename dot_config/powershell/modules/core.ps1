5df402e0ec2ee38e79649ae3225e98c93ad816c1
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-08-09 00:22:31 +0200

    lazy-load oh-my-posh

--- dot_config/powershell/modules/core.ps1
+++ dot_config/powershell/modules/core.ps1
@@ -22,35 +22,13 @@ function Get-Profile-Async {
     }
 }
 
-function Import-File {
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [ValidateNotNullOrEmpty()]
-        [string] $Path
-    )
-    try {
-        $resolvedPath = (Resolve-Path -LiteralPath $Path -ErrorAction Stop).Path
-        if (-not (Test-Path -LiteralPath $resolvedPath -PathType Leaf)) {
-            throw "File not found: $Path"
-        }
-
-        . $resolvedPath
-        Write-Verbose "Imported file: $resolvedPath"
-    }
-    catch {
-        $message = "Failed to import '$Path': $($_.Exception.Message)"
-        $global:ProfileIssues += $message
-    }
-}
-
-
 # Schedules module/script loading to occur asynchronously during idle time.
 # Each queued script is executed one-at-a-time on each idle event, which keeps
 # the initial prompt responsive.
 function Start-AsyncModuleInitialization {
     param(
         [Parameter(Mandatory)]
-        [hashtable] $FilePaths
+        [hashtable] $myModules
     )
     # Avoid double registration if called more than once
     if (Get-Variable -Name '__initQueue' -Scope Global -ErrorAction SilentlyContinue) {
@@ -58,13 +36,12 @@ function Start-AsyncModuleInitialization {
     }
 
     $global:__initQueue = New-Object System.Collections.Queue
-    foreach ($file in $FilePaths.Keys) { 
+    foreach ($file in $myModules.Keys) { 
         [void]$global:__initQueue.Enqueue(@{
                 Name = $file
-                Path = $FilePaths[$file]
+                Path = $myModules[$file]
             }) 
     }
-
 
     # Register idle handler; hidden from Get-EventSubscriber with -SupportEvent
     Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -SupportEvent -Action {
@@ -74,18 +51,20 @@ function Start-AsyncModuleInitialization {
                 $moduleTime = [System.Diagnostics.Stopwatch]::StartNew()
 
                 try {
+                    # Create Module and load it
                     New-Module -Name $file.Name -ScriptBlock { 
                         . $args[0]
                     } -ArgumentList $file.Path | Import-Module -Global -DisableNameChecking
+                    
+                    # Track it
+                    $elapesdmoduleMS = [math]::Round($moduleTime.Elapsed.TotalMilliseconds)
+                    $global:ProfileLoadedAsync += "$($file.Name) ($elapesdmoduleMS ms) from $($file.Path)"
                 }
                 catch {
+                    $moduleTime.Stop() 
                     $global:ProfileIssues += "Async init failed (${$file.Name}): $($_.Exception.Message)"
                     return
                 }
-
-                $moduleTime.Stop() 
-
-                $global:ProfileLoadedAsync += "Loaded $($file.Name) ($elapsedMs ms) from $($file.Path)"
             }
             else {
                 # Finished: unregister and cleanup
@@ -93,13 +72,13 @@ function Start-AsyncModuleInitialization {
 
                 # Re-render the prompt as soon as possible
                 try {
-                    # [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+                    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
                 }
                 catch {
                     # PSReadLine not available; ignore
                 }
 
-
+                # Display all errors
                 Get-Profile-Issues
             }
         }
