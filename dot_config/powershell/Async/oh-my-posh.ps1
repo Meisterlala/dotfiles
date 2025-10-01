355f30caf2d3228d04c12789e7cef903bb18be43
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-10-01 11:42:45 +0200

    Update .config/powershell/Async/oh-my-posh.ps1

--- dot_config/powershell/Async/oh-my-posh.ps1
+++ dot_config/powershell/Async/oh-my-posh.ps1
@@ -2,6 +2,18 @@
 $configPath = Resolve-Path (Join-Path $HOME ".config/oh-my-posh.yaml")
 oh-my-posh init --config $configPath pwsh | Invoke-Expression
 
+# re-add zoxide if needed
+$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction Ignore -ValueOnly)
+if ($global:__zoxide_hooked -eq 1) {
+    $global:__zoxide_prompt_old = $function:prompt
+
+    function global:prompt {
+        if ($null -ne $__zoxide_prompt_old) {
+            & $__zoxide_prompt_old
+        }
+        $null = __zoxide_hook
+    }
+}
 
 # Re-render the prompt as soon as possible
 try {
