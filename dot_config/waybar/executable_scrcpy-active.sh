142e9da068c94f5d6b3ca07f251a1c3876f671c5
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-11-02 17:40:05 +0100

    Update .config/waybar/scrcpy-active.sh

--- dot_config/waybar/executable_scrcpy-active.sh
+++ dot_config/waybar/executable_scrcpy-active.sh
@@ -4,16 +4,16 @@ set -Eeuo pipefail
 # Check if scrcpy is running and output status for waybar
 pid=$(pgrep -x scrcpy | head -1)
 if [ -n "$pid" ]; then
-    # Kill it!
-    if [ "${1-}" = "--kill" ]; then
-        kill "$pid"
-        echo ""
-        exit 0
-    fi
-
     # Get the command line arguments of the scrcpy process
     scrcpy_cmd=$(ps -p "$pid" -o args=)
 
+    # Kill it!
+    if [ "${1-}" = "--kill" ]; then
+        kill "$pid"
+        notify-send --icon=video-display "Killed scrcpy process" "Process ID $pid was terminated."
+        echo ""
+        exit 0
+    fi
 
     if echo "$scrcpy_cmd" | grep -q -- "--video-source=camera"; then
         echo "scrcpy  "
