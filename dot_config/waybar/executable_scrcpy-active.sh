3342afd7ba740839ff2a49cb026669ca15fadbc8
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-11-02 17:33:02 +0100

    Update .config/polybar/config.ini Update .config/waybar/config.jsonc Add .config/waybar/scrcpy-active.sh Update .config/waybar/style.css

---
+++ dot_config/waybar/executable_scrcpy-active.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# Check if scrcpy is running and output status for waybar
+pid=$(pgrep -x scrcpy | head -1)
+if [ -n "$pid" ]; then
+    # Kill it!
+    if [ "${1-}" = "--kill" ]; then
+        kill "$pid"
+        echo ""
+        exit 0
+    fi
+
+    # Get the command line arguments of the scrcpy process
+    scrcpy_cmd=$(ps -p "$pid" -o args=)
+
+
+    if echo "$scrcpy_cmd" | grep -q -- "--video-source=camera"; then
+        echo "scrcpy  "
+    else
+        echo "scrcpy "
+    fi
+else
+    echo ""
+fi
